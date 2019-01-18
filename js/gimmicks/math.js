f17df2104deae7b0d9adf43b81e9829443fb548a
Isana Kashiwai <isana.k@gmail.com>
2019-01-18 11:55:12 +0900

    Update MathJax CDN URL
    cdn.mathjax.org/mathjax/latest/MathJax.js
    -> cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js

--- js/gimmicks/math.js
+++ js/gimmicks/math.js
@@ -10,7 +10,7 @@
                 processEscapes: true
             }
         };
-        var url = $.md.prepareLink('cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML', { forceHTTP: true });
+        var url = $.md.prepareLink('cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS-MML_HTMLorMML', { forceHTTP: true });
         var script = document.createElement('script');
         script.src = url;
         document.getElementsByTagName('head')[0].appendChild(script);
